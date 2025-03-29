5724c75deb526d4eb09ca5a81731c181cc3b2d7d
Yves Roland Ouiya <ouiyayves@gmail.com>
2025-03-29 23:42:09 +0000

    Ajout d'image

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.utils.html import format_html
 from .models import Product, ProductImage
 
 class ProductImageInline(admin.TabularInline):
@@ -25,13 +26,18 @@ class HasImagesFilter(admin.SimpleListFilter):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'barcode', 'list_price', 'discount_price', 'stock_quantity', 'is_available', 'is_online', 'hide_price', 'created_at')
+    list_display = (
+        'name', 'barcode', 'list_price', 'discount_price', 'stock_quantity',
+        'is_available', 'is_online', 'hide_price',
+        'image_1920_preview', 'image_1024_preview', 'image_512_preview', 'image_256_preview', 'created_at'
+    )
     list_filter = ('is_available', 'is_online', 'hide_price', 'categ_id', 'brand', 'created_at', HasImagesFilter)
     search_fields = ('name', 'barcode', 'default_code', 'keywords', 'search_tags', 'description')
     prepopulated_fields = {'slug': ('name',)}
-    readonly_fields = ('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'image_1920_preview', 'image_1024_preview', 'image_512_preview', 'image_256_preview')
     list_editable = ('is_available', 'is_online', 'hide_price')
     inlines = [ProductImageInline]
+
     fieldsets = (
         ('Informations générales', {
             'fields': ('name', 'slug', 'barcode', 'default_code', 'odoo_id', 'categ_id', 'brand')
@@ -49,7 +55,7 @@ class ProductAdmin(admin.ModelAdmin):
             'fields': ('meta_title', 'meta_description', 'keywords', 'search_tags')
         }),
         ('Médias & Fichiers', {
-            'fields': ('image_url', 'image_1920', 'image_1024', 'image_512', 'image_256', 'video_url', 'pdf_brochure')
+            'fields': ('image_url', 'image_1920', 'image_1024', 'image_512', 'image_256', 'video_url', 'pdf_brochure', 'image_1920_preview', 'image_1024_preview', 'image_512_preview', 'image_256_preview')
         }),
         ('Dates importantes', {
             'fields': ('created_at', 'updated_at')
@@ -58,3 +64,25 @@ class ProductAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(image_count=Count('images'))
+
+    def image_preview(self, image_url, size_label):
+        if image_url:
+            source_label = "Odoo" if "http" in image_url else "Local"
+            return format_html('<img src="{}" width="50" height="50" style="border-radius:4px;"/> ({})', image_url, source_label)
+        return "Pas d'image"
+
+    def image_1920_preview(self, obj):
+        return self.image_preview(obj.image_1920, "1920")
+    image_1920_preview.short_description = "Image 1920"
+
+    def image_1024_preview(self, obj):
+        return self.image_preview(obj.image_1024, "1024")
+    image_1024_preview.short_description = "Image 1024"
+
+    def image_512_preview(self, obj):
+        return self.image_preview(obj.image_512, "512")
+    image_512_preview.short_description = "Image 512"
+
+    def image_256_preview(self, obj):
+        return self.image_preview(obj.image_256, "256")
+    image_256_preview.short_description = "Image 256"
