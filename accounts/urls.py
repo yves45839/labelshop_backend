ece2ccae321e3e592550d1c33cb02ed110e28906
yves45839 <ouiyayves@gmail.com>
2025-05-27 15:47:08 +0000

    Add login, logout and account cancel endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, login_user, logout_user, cancel_account
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('cancel/', cancel_account, name='cancel_account'),
 ]
