019fc64465271d1e0c7c6d1bb96159c66556fdc9
yves45839 <ouiyayves@gmail.com>
2025-06-09 13:12:00 +0200

    Read secret key from env

--- labelshop_backend/settings.py
+++ labelshop_backend/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i!k3jzi@6*&luy^b_izz)ap-)@9c&k&wly-k-t7i!zl#!26c39'
+# The secret key is now expected to be provided via the
+# DJANGO_SECRET_KEY environment variable.
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
