ac7b2b7f87a17b491fcce844f5c7c294786baab5
yves45839 <ouiyayves@gmail.com>
2025-12-01 11:45:56 +0000

    cat ajoutée

--- products/management/commands/classifier.py
+++ products/management/commands/classifier.py
@@ -2,83 +2,119 @@ import re
 from typing import Optional, Tuple
 
 from django.core.management.base import BaseCommand
-from products.models import Product  # adapte si ton modèle est ailleurs
+from products.models import Product
 
 # ------------------------------------------------------------------ #
-#   RÈGLES HIKVISION (famille, catégorie, type)                      #
+#   REGLES HIKVISION (famille, categorie, type)                      #
 # ------------------------------------------------------------------ #
 
 HIK_RULES: list[Tuple[str, Tuple[str, str, Optional[str]]]] = [
-    # 1. Interphonie & contrôle d’accès
-    (r"^I?DS-K1T", ("Contrôle d'accès", "Terminaux autonomes", "Terminal autonome")),
-    (r"^I?DS-K1A", ("Contrôle d'accès", "Temps de présence", "Terminal de pointage")),
-    (r"^I?DS-K2",  ("Contrôle d'accès", "Contrôleurs & modules", "Contrôleur de porte")),
-    (r"^I?DS-K3",  ("Contrôle d'accès", "Portillons & tourniquets", None)),
-    (r"^I?DS-K4",  ("Contrôle d'accès", "Serrures & ventouses", None)),
+    # 1. Interphonie & controle d'acces
+    (r"^I?DS-K1T", ("Controle d'acces", "Terminaux autonomes", "Terminal autonome")),
+    (r"^I?DS-K1A", ("Controle d'acces", "Temps de presence", "Terminal de pointage")),
+    (r"^I?DS-K2", ("Controle d'acces", "Controleurs & modules", "Controleur de porte")),
+    (r"^I?DS-K3", ("Controle d'acces", "Portillons & tourniquets", None)),
+    (r"^I?DS-K4", ("Controle d'acces", "Serrures & ventouses", None)),
+    (r"^I?DS-KAS", ("Controle d'acces", "Kits & accessoires", None)),
 
-    (r"^I?DS-KH", ("Interphonie", "Moniteurs intérieurs", "Moniteur intérieur")),
+    (r"^I?DS-KH", ("Interphonie", "Moniteurs interieurs", "Moniteur interieur")),
     (r"^I?DS-KV", ("Interphonie", "Platines de rue & doorbells", "Platine de rue villa")),
     (r"^I?DS-KD", ("Interphonie", "Platines de rue & doorbells", "Platine modulaire")),
-    (r"^I?DS-KB", ("Interphonie", "Platines de rue & doorbells", "Sonnette vidéo")),
+    (r"^I?DS-KB", ("Interphonie", "Platines de rue & doorbells", "Sonnette video")),
+    (r"^I?DS-KIS", ("Interphonie", "Kits video", None)),
 
     # 2. Alarmes intrusion
     (r"^I?DS-PWA", ("Alarme intrusion", "Centrales", "Centrale AX PRO")),
-    (r"^I?DS-PD",  ("Alarme intrusion", "Détecteurs / contacts / sirènes", "Détecteur / contact")),
-    (r"^I?DS-PS",  ("Alarme intrusion", "Détecteurs / contacts / sirènes", "Sirène")),
-    (r"^I?DS-PM",  ("Alarme intrusion", "Périphériques", "Module / expander")),
-    (r"^I?DS-P",   ("Alarme intrusion", "AX PRO & périphériques", None)),
+    (r"^I?DS-PD", ("Alarme intrusion", "Detecteurs / contacts / sirenes", "Detecteur / contact")),
+    (r"^I?DS-PS", ("Alarme intrusion", "Detecteurs / contacts / sirenes", "Sirene")),
+    (r"^I?DS-PM", ("Alarme intrusion", "Peripheriques", "Module / expander")),
+    (r"^I?DS-P", ("Alarme intrusion", "AX PRO & peripheriques", None)),
 
-    # 3. Vidéo – caméras
-    (r"^I?DS-2CD63", ("Vidéosurveillance IP", "Caméras IP", "Caméra fisheye IP")),
-    (r"^I?DS-2DP",   ("Vidéosurveillance IP", "Caméras panoramiques", "PanoVu")),
+    # 3. Video - cameras
+    (r"^I?DS-2CD63", ("Videosurveillance IP", "Cameras IP", "Camera fisheye IP")),
+    (r"^I?DS-2DP", ("Videosurveillance IP", "Cameras panoramiques", "PanoVu")),
+    (r"^I?DS-2CD", ("Videosurveillance IP", "Cameras IP", "Camera IP fixe")),
+    (r"^I?DS-2CV", ("Videosurveillance IP", "Cameras IP", "Camera IP Wi-Fi")),
+    (r"^I?DS-2XS", ("Videosurveillance IP", "Cameras IP", "Camera IP solaire")),
+    (r"^(I?DS-2CE|DS-2CS|DS-2CC)", ("Videosurveillance analogique", "Cameras analogiques", "Camera TurboHD")),
+    (r"^I?DS-2DF", ("Videosurveillance IP", "Cameras PTZ", "Camera PTZ IP")),
+    (r"^I?DS-2DE", ("Videosurveillance IP", "Cameras PTZ", "Camera PTZ IP")),
+    (r"^I?DS-2AE", ("Videosurveillance analogique", "Cameras PTZ", "Camera PTZ analogique")),
+    (r"^I?DS-2TD", ("Videosurveillance specialisee", "Cameras thermiques", "Camera thermique")),
+    (r"^I?DS-2TP", ("Videosurveillance specialisee", "Cameras thermiques", "Camera thermique portative")),
+    (r"^I?DS-2TR", ("Videosurveillance specialisee", "Cameras thermiques", "Lunette / scope thermique")),
+    (r"^I?DS-2", ("Videosurveillance IP", "Cameras IP", "Camera IP fixe")),  # fallback Hikvision DS-2..
+    (r"^CS-", ("Videosurveillance IP", "Cameras IP", None)),
 
-    (r"^I?DS-2CD", ("Vidéosurveillance IP", "Caméras IP", "Caméra IP fixe")),
-    (r"^I?DS-2CV", ("Vidéosurveillance IP", "Caméras IP", "Caméra IP Wi-Fi")),
-    (r"^I?DS-2XS", ("Vidéosurveillance IP", "Caméras IP", "Caméra IP solaire")),
+    # 4. Video - enregistreurs
+    (r"^I?DS-7\d{2}(HGHI|HQHI|HUHI|HTHI)", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^I?DS-71\d{2}(HGHI|HQHI|HUHI|HTHI)", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^I?DS-72", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^I?DS-73", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^I?DS-81", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^I?DS-90", ("Enregistreurs", "DVR analogiques", "DVR hybride")),
+    (r"^I?DS-7(1|6|7|9)\d{2}N(XI)?", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^I?DS-76", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^I?DS-77", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^I?DS-86", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^I?DS-96", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^DS-E.*(HGHI|HQHI)", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
+    (r"^DS-E.*NI", ("Enregistreurs", "NVR IP", "NVR IP")),
+    (r"^DS-J\d+.*HGHI", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
 
-    (r"^(I?DS-2CE|DS-2CS|DS-2CC)", ("Vidéosurveillance analogique", "Caméras analogiques", "Caméra TurboHD")),
-
-    (r"^I?DS-2DF", ("Vidéosurveillance IP", "Caméras PTZ", "Caméra PTZ IP")),
-    (r"^I?DS-2DE", ("Vidéosurveillance IP", "Caméras PTZ", "Caméra PTZ IP")),
-    (r"^I?DS-2AE", ("Vidéosurveillance analogique", "Caméras PTZ", "Caméra PTZ analogique")),
-
-    (r"^I?DS-2TD", ("Vidéosurveillance spécialisée", "Caméras thermiques", "Caméra thermique")),
-    (r"^I?DS-2TP", ("Vidéosurveillance spécialisée", "Caméras thermiques", "Caméra thermique portative")),
-    (r"^I?DS-2TR", ("Vidéosurveillance spécialisée", "Caméras thermiques", "Lunette / scope thermique")),
-
-    # 4. Vidéo – enregistreurs
-    (r"^DS-7\d{2}(HGHI|HQHI|HUHI|HTHI)", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
-    (r"^DS-71\d{2}(HGHI|HQHI|HUHI|HTHI)", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
-    (r"^DS-72", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
-    (r"^DS-73", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
-    (r"^DS-81", ("Enregistreurs", "DVR analogiques", "DVR Turbo HD")),
-    (r"^DS-90", ("Enregistreurs", "DVR analogiques", "DVR hybride")),
-
-    (r"^DS-7(1|6|7|9)\d{2}N(XI)?", ("Enregistreurs", "NVR IP", "NVR IP")),
-    (r"^DS-76", ("Enregistreurs", "NVR IP", "NVR IP")),
-    (r"^DS-77", ("Enregistreurs", "NVR IP", "NVR IP")),
-    (r"^DS-86", ("Enregistreurs", "NVR IP", "NVR IP")),
-    (r"^DS-96", ("Enregistreurs", "NVR IP", "NVR IP")),
-
-    # 5. Réseau / affichage / stockage
-    (r"^DS-3E", ("Réseau & transmission", "Switches PoE", "Switch réseau")),
-    (r"^DS-D",  ("Affichage & mur d’images", "Moniteurs / vidéo-wall / LED", None)),
-
+    # 5. Reseau / affichage / stockage
+    (r"^DS-3E", ("Reseau & transmission", "Switches PoE", "Switch reseau")),
+    (r"^DS-3T", ("Reseau & transmission", "Switches PoE", "Switch reseau")),
+    (r"^DS-3W", ("Reseau & transmission", "Equipements reseau", None)),
+    (r"^DS-69", ("Affichage & mur d'images", "Decoders / controleurs mur", None)),
+    (r"^DS-D", ("Affichage & mur d'images", "Moniteurs / video-wall / LED", None)),
     (r"^DS-A7", ("Stockage", "Serveurs de stockage", "Hybrid SAN / iSCSI")),
     (r"^DS-A8", ("Stockage", "Serveurs de stockage", "Hybrid SAN / iSCSI")),
-    (r"^HS-A",  ("Stockage", "Serveurs de stockage", "NAS / stockage IP")),
+    (r"^HS-A", ("Stockage", "Serveurs de stockage", "NAS / stockage IP")),
+    (r"^DS-UPS", ("Accessoires generaux", "Alimentation & UPS", "Onduleur")),
 
-    (r"^DS-1[0-9]", ("Accessoires", "Câbles & transmission", None)),
-    (r"^DS-12",    ("Accessoires", "Supports & boîtiers", None)),
-    (r"^DS-13",    ("Accessoires", "Supports & boîtiers", None)),
+    # Cables / supports
+    (r"^DS-1L", ("Reseau & transmission", "Cables reseau", "Cable UTP/FTP")),
+    (r"^DS-1", ("Accessoires", "Cables & transmission", None)),
+    (r"^DS-12", ("Accessoires", "Supports & boitiers", None)),
+    (r"^DS-13", ("Accessoires", "Supports & boitiers", None)),
+    (r"^DSK4", ("Controle d'acces", "Serrures & ventouses", None)),
 
     # 6. Trafic / parking
-    (r"^DS-TMG", ("Trafic / Parking", "Gestion d’accès véhicules", "Radar entrée/sortie")),
-    (r"^DS-TCG", ("Trafic / Parking", "Reconnaissance de plaques", "Caméra ANPR")),
+    (r"^DS-TMG", ("Trafic / Parking", "Gestion d'acces vehicules", "Radar entree/sortie")),
+    (r"^DS-TCG", ("Trafic / Parking", "Reconnaissance de plaques", "Camera ANPR")),
 
-    # 7. Accessoires généraux (HDD, connectique…)
-    (r"^(3TO|4TO|6TERA|1TO|2TO|500GO)", ("Stockage", "Disques durs", "HDD vidéosurveillance")),
-    (r"^(BNC|DC|RCA)", ("Accessoires généraux", "Câbles & connectique", "Connecteurs & fiches")),
+    # 7. Accessoires generaux (HDD, connectique…)
+    (r"^CAT *6", ("Accessoires generaux", "Cables & connectique", "Cable reseau CAT6")),
+    (r"^CAT *7", ("Accessoires generaux", "Cables & connectique", "Cable reseau CAT7")),
+    (r".*CAT *6", ("Accessoires generaux", "Cables & connectique", "Cable reseau CAT6")),
+    (r".*CAT *7", ("Accessoires generaux", "Cables & connectique", "Cable reseau CAT7")),
+    (r"RG59|RJ59", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"COXIAL|COAXIAL", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"^EV-CA", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"^NC634", ("Accessoires generaux", "Cables & connectique", "Cable reseau CAT6")),
+    (r"^COFFRET", ("Accessoires generaux", "Supports & boitiers", None)),
+    (r"^EXTENDER", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"GACHE", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"GTECH", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"MICRO", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"^LKV", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"^HDD", ("Stockage", "Disques durs", None)),
+    (r"^DISQUEDUR", ("Stockage", "Disques durs", None)),
+    (r"^ST\d+VX", ("Stockage", "Disques durs", None)),
+    (r"^(3TO|4TO|6TERA|1TO|2TO|500GO)", ("Stockage", "Disques durs", "HDD videosurveillance")),
+    (r"^(BNC|DC|RCA)", ("Accessoires generaux", "Cables & connectique", "Connecteurs & fiches")),
+    (r"CONNECTEUR", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"CONNECTEURS", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"CG577", ("Accessoires generaux", "Cables & connectique", None)),
+    (r"^DS-KP", ("Interphonie", "Accessoires", None)),
+    (r"^NK\d+", ("Enregistreurs", "NVR IP", "Kit NVR IP")),
+    (r"^OUTDOOR.*FTP", ("Accessoires generaux", "Cables & connectique", "Cable reseau")),
+    (r"^HF-S", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"^IC ?S50", ("Accessoires generaux", "Badges & cartes", None)),
+    (r"^S50\+TK4100", ("Controle d'acces", "Badges & cartes", "Carte RFID/MIFARE")),
+    (r"^NP-SH", ("Accessoires generaux", "Accessoires divers", None)),
+    (r"^DS-H", ("Accessoires generaux", "Accessoires divers", None)),
 ]
 
 
@@ -87,13 +123,13 @@ def infer_categories(
     existing_main: Optional[str] = None,
     existing_sub: Optional[str] = None,
 ) -> Tuple[Optional[str], Optional[str], Optional[str]]:
-    """
-    Retourne (famille, catégorie, type) pour une référence Hikvision.
-    """
+    """Retourne (famille, categorie, type) pour une reference Hikvision."""
     if not default_code:
         return existing_main, existing_sub, None
 
-    normalized_code = default_code.strip().upper().replace(" ", "")
+    normalized_code = str(default_code).strip().upper().replace(" ", "")
+    if normalized_code in {"FALSE", "NONE"}:
+        return existing_main, existing_sub, None
 
     for pattern, (main, sub, type_) in HIK_RULES:
         if re.match(pattern, normalized_code):
@@ -107,13 +143,11 @@ def classify(
     existing_main: Optional[str] = None,
     existing_sub: Optional[str] = None,
 ):
-    """
-    Retourne une liste [famille, catégorie, type] pour un SKU donné.
-    """
+    """Retourne une liste [famille, categorie, type] pour un SKU donne."""
     main, sub, type_ = infer_categories(sku_raw, existing_main, existing_sub)
 
     if not main and not sub:
-        return ["Non classé"]
+        return ["Non classe"]
 
     return [main, sub, type_]
 
@@ -122,6 +156,7 @@ def classify(
 #   COMMANDE DJANGO                                                  #
 # ------------------------------------------------------------------ #
 
+
 class Command(BaseCommand):
     help = "Applique la classification Hikvision aux produits"
 
@@ -129,46 +164,77 @@ class Command(BaseCommand):
         parser.add_argument(
             "--dry-run",
             action="store_true",
-            help="Affiche les résultats sans sauvegarder en base",
+            help="Affiche les resultats sans sauvegarder en base",
         )
 
     def handle(self, *args, **options):
         dry_run = options["dry_run"]
 
-        # tu peux filtrer sur la marque si tu veux :
         qs = Product.objects.all()
-        # qs = Product.objects.filter(brand__name__icontains="hikvision")
-
         self.stdout.write(f"Nombre de produits : {qs.count()}")
 
+        updated = 0
+        skipped_no_ref = 0
+        no_match = 0
+        unchanged = 0
+
         for product in qs:
-            ref = (getattr(product, "default_code", "") or getattr(product, "sku", "") or "").strip()
-            if not ref:
+            ref_raw = (
+                getattr(product, "default_code", None)
+                or getattr(product, "sku", None)
+                or getattr(product, "barcode", None)
+                or getattr(product, "slug", None)
+                or getattr(product, "name", None)
+                or ""
+            )
+            ref = str(ref_raw).strip()
+            if not ref or ref.lower() in {"false", "none"}:
+                skipped_no_ref += 1
                 continue
 
-            # on suppose que ton modèle a ces champs :
             existing_main = getattr(product, "category_main", None)
             existing_sub = getattr(product, "category_sub", None)
 
-            main, sub, type_ = infer_categories(ref, existing_main, existing_sub)
+            def _clean_existing(value: Optional[str]) -> Optional[str]:
+                if isinstance(value, str) and "non class" in value.lower():
+                    return None
+                return value
 
+            existing_main = _clean_existing(existing_main)
+            existing_sub = _clean_existing(existing_sub)
+
+            main, sub, type_ = infer_categories(ref, existing_main, existing_sub)
             self.stdout.write(f"{ref} -> {main} / {sub} / {type_}")
 
-            if not dry_run:
-                if hasattr(product, "category_main"):
-                    product.category_main = main
-                if hasattr(product, "category_sub"):
-                    product.category_sub = sub
-                if hasattr(product, "category_type"):
-                    product.category_type = type_
+            if not main and not sub:
+                no_match += 1
+                continue
 
-                fields = []
-                if hasattr(product, "category_main"):
-                    fields.append("category_main")
-                if hasattr(product, "category_sub"):
-                    fields.append("category_sub")
-                if hasattr(product, "category_type"):
-                    fields.append("category_type")
+            if dry_run:
+                continue
 
-                if fields:
-                    product.save(update_fields=fields)
+            fields: list[str] = []
+            if hasattr(product, "category_main") and getattr(product, "category_main") != main:
+                product.category_main = main
+                fields.append("category_main")
+            if hasattr(product, "category_sub") and getattr(product, "category_sub") != sub:
+                product.category_sub = sub
+                fields.append("category_sub")
+            if hasattr(product, "category_type") and getattr(product, "category_type") != type_:
+                product.category_type = type_
+                fields.append("category_type")
+
+            if fields:
+                product.save(update_fields=fields)
+                updated += 1
+            else:
+                unchanged += 1
+
+        if dry_run:
+            self.stdout.write(self.style.WARNING("Dry-run : aucune modification en base."))
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Termine. MAJ={updated}, inchanges={unchanged}, sans_ref={skipped_no_ref}, non_matches={no_match}."
+            )
+        )
